274992b885e9f85d9dd8f7087b345b21e57bf618
Linda Ojinnaka <lindaojinnaka@gmail.com>
2020-05-14 12:26:23 +1000

    changes

--- src/pet_shop/app/controllers/dogs_controller.rb
+++ src/pet_shop/app/controllers/dogs_controller.rb
@@ -12,16 +12,25 @@ def index
 end
 
 def new 
-
-
+    @dog = Dog.new   
 end 
 
 def create
-
+    p "************"
+    p params 
+    p "************"
+    current_user.dogs.create(dog_params)
+    redirect_dogs_path
 end
 
 def show
 
 end 
 
+private 
+def dog_params
+    params.require(:dog).permit(:title, :description, :breed, :age, :state, :city, :potty_trained, :likes_dogs, :likes_children, :picture, :name)
 end 
+
+end 
+
--- src/pet_shop/db/seeds.rb
+++ src/pet_shop/db/seeds.rb
@@ -2,7 +2,7 @@ user = User.create(email:"lindaojinnaka@gmail.com", password: "password")
 
 for i in 0..10
 dog = user.dogs.create(
-    title:Faker::Creature::Dog.name,
+    name:Faker::Creature::Dog.name
     description:Faker::Lorem.paragraph,
     age:Faker::Number.number(digits: 1),
     potty_trained:true,
@@ -10,7 +10,6 @@ dog = user.dogs.create(
     likes_children:true,
     state:Faker::Address.state,
     city:Faker::Address.city,
-    name:Faker::Creature::Dog.name
 )
 
 end
